fa6ac971e8a0f38b315fbe07f364d4f835966091
bexearn12 <earnshr@gmail.com>
2021-06-14 20:26:37 -0500

    commit tree 00cef99e50ec726d4041d54b242ac62e377842d8

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const cats = ["Milo", "Otis", "Garfield"]
+// index.js
+// cats
+//   1) is assigned an initial value of ["Milo", "Otis", "Garfield"]
+
+let cats = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendCat(name) {
     cats.push(name)
@@ -8,16 +12,16 @@ function destructivelyPrependCat(name) {
     cats.unshift(name)
 }
 
-function destructivelyRemoveLastCat() {
-    cats.pop()
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
 }
 
-function destructivelyRemoveFirstCat() {
-    cats.shift()
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
 }
 
 function appendCat(name) {
-    return [...cats, name]
+     return [...cats, name]
 }
 
 function prependCat(name) {
@@ -25,14 +29,11 @@ function prependCat(name) {
 }
 
 function removeLastCat() {
-    return cats.slice(0, cats.length - 1)
+    return cats.slice(0, cats.length -1 )
 }
 
 function removeFirstCat() {
     return cats.slice(1)
 }
-
-
-
 
 
